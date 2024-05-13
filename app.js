70424f36eae1e5842d819046d9d56e9e3c3c0965
legendofnoobs <154618805+legendofnoobs@users.noreply.github.com>
2024-05-13 23:34:21 +0300

    Advice generator app challenged by frontend mentor

---
+++ app.js
@@ -0,0 +1,28 @@
+const URL = "https://api.adviceslip.com/advice";
+
+const adviceId = document.querySelector("#adv-id");
+const adviceText = document.querySelector("#adv-text");
+const btn = document.querySelector(".dice-container");
+
+getAdvice();
+
+btn.addEventListener("click", function() {
+    diceRollAnimation(btn);
+    getAdvice();
+});
+
+async function getAdvice() {
+    const response = await fetch(URL);
+    const data = await response.json();
+
+    adviceId.innerText = data.slip.id;
+    adviceText.innerText = `“${data.slip.advice}”`;
+}
+
+const diceRollAnimation = (btn) => {
+    btn.classList.add("rotate");
+
+    setTimeout(() => {
+    btn.classList.remove("rotate");
+    }, 400);
+};
